5f61bf748aa5909bcf43e81dac10fbdfe6e7b7fe
Brian Ting <ting779@gmail.com>
2019-10-10 14:32:15 +0800

    Can download the latest Opera version + attempt to embed upd info

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./pkg2appimage opera.yml
+chmod a+x pkg2appimage-continuous-x86_64.AppImage appimagetool-x86_64.AppImage 
+./pkg2appimage-continuous-x86_64.AppImage opera.yml
 
 cd out/
 mv Opera-*.AppImage Opera-x86_64.AppImage
-zsyncmake *.AppImage
