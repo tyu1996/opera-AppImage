b450ceab5191d41909f3053e03dc3cf8ec111c70
Brian Ting <ting779@gmail.com>
2019-10-12 11:12:36 +0800

    Fix build error and implements zsync update

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 chmod a+x pkg2appimage-continuous-x86_64.AppImage appimagetool-x86_64.AppImage 
 ./pkg2appimage-continuous-x86_64.AppImage opera.yml
+./appimagetool-x86_64.AppImage -u "gh-releases-zsync|tyu1996|opera-AppImage|continuous|Opera-x86_64.AppImage.zsync" opera/opera.AppDir
 
-cd out/
-mv Opera-*.AppImage Opera-x86_64.AppImage
+mkdir out
+mv Opera-* out/
