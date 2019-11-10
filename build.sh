fd17d02444ab3f261a4683aa13146da1a4ea6930
Brian <ting779@gmail.com>
2019-11-10 22:58:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod a+x pkg2appimage-continuous-x86_64.AppImage appimagetool-x86_64.AppImage 
 ./pkg2appimage-continuous-x86_64.AppImage opera.yml
-./appimagetool-x86_64.AppImage -u "gh-releases-zsync|tyu1996|opera-AppImage|continuous|Opera-x86_64.AppImage.zsync" opera/opera.AppDir
+./appimagetool-x86_64.AppImage -u "gh-releases-zsync|tyu1996|opera-AppImage|continuous|Opera-x86_64.AppImage.zsync" opera-stable/opera-stable.AppDir
 
 mkdir out
 mv Opera-* out/
