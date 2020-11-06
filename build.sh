04f594c7c886fbb9c7d3b2081165b9075e47fbf0
Brian <ting779@gmail.com>
2020-11-06 22:52:59 +0800

    Update pkg2appimage name

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-chmod a+x pkg2appimage-continuous-x86_64.AppImage appimagetool-x86_64.AppImage 
-./pkg2appimage-continuous-x86_64.AppImage opera.yml
+chmod a+x pkg2appimage-1795-x86_64.AppImage appimagetool-x86_64.AppImage 
+./pkg2appimage-1795-x86_64.AppImage opera.yml
 ./appimagetool-x86_64.AppImage -u "gh-releases-zsync|tyu1996|opera-AppImage|continuous|Opera*.AppImage.zsync" opera-stable/opera-stable.AppDir
 
 mkdir out
